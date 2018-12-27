f6ea72567302ce170cd00fd0e50234e58f0fec90
Ryan <franciscogutierrez@franciscos-mbp.home>
2018-12-27 12:37:38 -0800

    122718

---
+++ assets/javascript/gif.js
@@ -0,0 +1,3 @@
+var sprotTeams = ["duke basketball", "dallas cowboys", "la lakers", "san diego padres", "colorado avalanch", "usc football", "oklahoma state football"];
+var url = "http://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=Zt5D0Wjk7gYFB2fVpKH8KXysPwnOhyCt&limit=10";
+var name = ""
